cda527b8412a7e2fc3cce6e71376639ec3bac074
siddhaarthh <siddharthjain418@gmail.com>
2024-04-12 22:35:11 +0530

    second

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,7 +6,7 @@ import todoRoutes from './routes'
 
 const app: Express = express()
 
-const PORT: string | number = process.env.PORT || 4000
+const PORT: string | number = process.env.PORT || 8080
 
 // app.use(bodyParser())
 app.use(cors())
@@ -14,13 +14,13 @@ app.use(todoRoutes)
 
 // replace the host "cluster0.xo006.mongodb.net" with the address of your host generated in MongoDB Atlas.
 // https://docs.mongodb.com/manual/reference/connection-string/
-const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.xo006.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+const uri: string = `mongodb+srv://sidjainsiddharth11:7oHWdWv8Zut5wigx@cluster0.zrbqmq0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
 mongoose.set('useFindAndModify', false)
 
 mongoose
-.connect(uri, options)
+.connect(uri + options)
 .then(() =>
     app.listen(PORT, () =>
         console.log(`Server running on http://localhost:${PORT}`)
--- server/src/types/todo.ts
+++ server/src/types/todo.ts
@@ -3,7 +3,7 @@ import { Document } from "mongoose"
 // a Todo interface that extends the Document type provided by mongoose.
 // https://mongoosejs.com/docs/documents.html
 export interface ITodo extends Document {
-    name: string
-    description: string
-    status: boolean
+    name: String,
+    description: String,
+    status: Boolean,
 }
